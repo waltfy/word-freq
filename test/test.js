5e97395d8ce733d0623ef455e9d4f520b27b305c
Walter Carvalho <waltervascarvalho@gmail.com>
2014-05-11 20:44:54 +0100

    added stopwords functionality

--- test/test.js
+++ test/test.js
@@ -1,94 +1,170 @@
 var t = require('tap').test;
 var wf = require('../');
 
-t('tokenisation test with a simple string', function (t) {
+var STR_A = "you're simply a test, a mere test";
+var STR_B = "you're simply a simplistic house, made for housing";
+var STR_C = "@waltercfilho tweeted about houses: housing is the most expensive thing ever f#!*";
 
-	var str = "you're simply a test, a mere test";
-	var strAlt = "you're simply a simplistic house, made for housing";
-
+t('tokenisation, excluding stop words', function (t) {
 	t.deepEqual(
-		wf.tokenise(str),
-		['you', 're', 'simply', 'a', 'test', 'a', 'mere', 'test'],
-		'arrays are the same'
+		wf.tokenise(STR_A),
+		['simply', 'test', 'mere', 'test'],
+		'wf.tokenise(STR_A)'
 	);
 
 	t.deepEqual(
-		wf.stem(strAlt),
-		[ "you", "re", "simpli", "a", "simplist", "hous", "made", "for", "hous" ],
-		'stemmer behaves as expected'	
-	);
-
-	t.end()
-});
-
-t('tokenisation test with a more complex string', function (t) {
-
-	var str = "@waltercfilho tweeted about houses: housing is the most expensive thing ever f#!*";
-
-	t.deepEqual(
-		wf.tokenise(str),
-		[ "waltercfilho", "tweeted", "about", "houses", "housing", "is", "the", "most", "expensive", "thing", "ever", "f" ],
-		'arrays are the same'
+		wf.tokenise(STR_B),
+		['simply', 'simplistic', 'house', 'housing'],
+		'wf.tokenise(STR_B)'
 	);
 
 	t.deepEqual(
-		wf.stem(str),
-		[ "waltercfilho", "tweet", "about", "hous", "hous", "is", "the", "most", "expens", "thing", "ever", "f" ],
-		'stemmer behaves as expected'	
-	);
-
-	t.end()
-});
-
-t('word frequency testing with stemmer off', function (t) {
-	var str = "@waltercfilho tweeted about houses: housing is the most expensive thing ever f#!* @phuunet @waltercfilho house #move";
-
-	t.deepEqual(
-		wf.freq(str, false),
-		{
-      "waltercfilho": 2,
-      "tweeted": 1,
-      "about": 1,
-      "houses": 1,
-      "housing": 1,
-      "is": 1,
-      "the": 1,
-      "most": 1,
-      "expensive": 1,
-      "thing": 1,
-      "ever": 1,
-      "f": 1,
-      "phuunet": 1,
-      "house": 1,
-      "move": 1
-    },
-		'word frequency is correct'
+		wf.tokenise(STR_C),
+		["waltercfilho", "tweeted", "houses", "housing", "expensive"],
+		'wf.tokenise(STR_C)'
 	);
 
 	t.end();
 });
 
-t('word frequency testing with stemmer off', function (t) {
-	var str = "@waltercfilho tweeted about houses: housing is the most expensive thing ever f#!* @phuunet @waltercfilho house #move";
+t('tokenisation, including stop words', function (t) {
+	
+	t.deepEqual(
+		wf.tokenise(STR_A, false),
+		['you', 're', 'simply', 'a', 'test', 'a', 'mere', 'test'],
+		'wf.tokenise(STR_A, false)'
+	);
 
 	t.deepEqual(
-		wf.freq(str, true),
+		wf.tokenise(STR_B, false),
+		["you", "re", "simply", "a", "simplistic", "house", "made", "for", "housing"],
+		'wf.tokenise(STR_B, false)'
+	);
+
+	t.deepEqual(
+		wf.tokenise(STR_C, false),
+		["waltercfilho", "tweeted", "about", "houses", "housing", "is", "the", "most", "expensive", "thing", "ever", "f"],
+		'wf.tokenise(STR_C, false)'
+	);
+
+	t.end();
+});
+
+t('stemming, excluding stop words', function (t) {
+
+	t.deepEqual(
+		wf.stem(STR_A),
+		['simpli', 'test', 'mere', 'test'],
+		'wf.stem(STR_A)'
+	);
+
+	t.deepEqual(
+		wf.stem(STR_B),
+		["simpli", "simplist", "hous", "hous"],
+		'wf.stem(STR_B)'
+	);
+
+	t.deepEqual(
+		wf.stem(STR_C),
+		["waltercfilho", "tweet", "hous", "hous", "expens"],
+		'wf.stem(STR_B)'
+	);
+
+	t.end();
+});
+
+t('stemming, including stop words', function (t) {
+	
+	t.deepEqual(
+		wf.stem(STR_A, false),
+		['you', 're', 'simpli', 'a', 'test', 'a', 'mere', 'test'],
+		'wf.stem(STR_A, false)'
+	);
+
+	t.deepEqual(
+		wf.stem(STR_B, false),
+		["you", "re", "simpli", "a", "simplist", "hous", "made", "for", "hous"],
+		'wf.stem(STR_B, false)'
+	);
+
+	t.deepEqual(
+		wf.stem(STR_C, false),
+		["waltercfilho", "tweet", "about", "hous", "hous", "is", "the", "most", "expens", "thing", "ever", "f"],
+		'wf.stem(STR_C, false)'
+	);
+
+	t.end();
+});
+
+t('word frequency testing, excluding stop words', function (t) {
+
+	/* with stemming */
+	t.deepEqual(
+		wf.freq(STR_C, true, true),
 		{
-      "waltercfilho" : 2,
+      "waltercfilho" : 1,
+      "tweet" : 1,
+      "hous" : 2,
+      "expens" : 1
+    },
+		'wf.freq(STR_C, true, true)'
+	);
+
+	/* without stemming */
+	t.deepEqual(
+		wf.freq(STR_C, true, false),
+		{
+			"waltercfilho" : 1,
+      "tweeted" : 1,
+      "houses" : 1,
+      "housing" : 1,
+      "expensive" : 1
+		},
+		'wf.freq(STR_C, true, false)'
+	);
+
+	t.end();
+});
+
+t('word frequency testing, including stop words', function (t) {
+
+	/* with stemming */
+	t.deepEqual(
+		wf.freq(STR_C, false, true),
+		{
+      "waltercfilho" : 1,
       "tweet" : 1,
       "about" : 1,
-      "hous" : 3,
+      "hous" : 2,
       "is" : 1,
       "the" : 1,
       "most" : 1,
       "expens" : 1,
       "thing" : 1,
       "ever" : 1,
-      "f" : 1,
-      "phuunet" : 1,
-      "move" : 1
+      "f" : 1
     },
-		'word frequency is correct'
+		'wf.freq(STR_C, false, true)'
+	);
+
+	/* without stemming */
+	t.deepEqual(
+		wf.freq(STR_C, false, false),
+		{
+			"waltercfilho" : 1,
+      "tweeted" : 1,
+      "about" : 1,
+      "houses" : 1,
+      "housing" : 1,
+      "is" : 1,
+      "the" : 1,
+      "most" : 1,
+      "expensive" : 1,
+      "thing" : 1,
+      "ever" : 1,
+      "f" : 1
+		},
+		'wf.freq(STR_C, false, false)'
 	);
 
 	t.end();
